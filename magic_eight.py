d7b594e7508a653400c2be9692f47d1c23b7db74
Riley <rilmaher@umich.edu>
2018-01-30 18:37:56 -0500

    check_question

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,5 @@
-input("What is your question? ")
+pop=input("What is your question? ")
+while pop!="quit":
+    pop=input("What is your question? ")
+    if "?" not in pop:
+        print("Sorry, I can only answer questions.")
